596b15e1333ae19d84c452e4596e8c75994d36df
FScoward <endlick@gmail.com>
2021-07-03 15:42:53 +0900

    :construction: 環境変数から値の読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use serde::Deserialize;
+
 fn main() {
     println!("Hello, world!");
+    match envy::prefixed("FITBIT_").from_env::<OAuth>() {
+        Ok(oauth) => println!("{:#?}", oauth),
+        Err(error) => panic!("{:#?}", error),
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct OAuth {
+    client_id: String,
+    client_secret: String,
 }
