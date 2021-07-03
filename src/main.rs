a88f45435daffd2ed622be86b3f1f8b5261bf12f
FScoward <endlick@gmail.com>
2021-07-04 00:32:09 +0900

    :recycle: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use serde::Deserialize;
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
-    let uri = envy::prefixed("FITBIT_").from_env::<OAuth>().map(|oauth_config| 
+    let uri = gen_get_code();
+    println!("{:?}", uri);
+
+
+
+
+}
+
+fn gen_get_code() -> Result<String, envy::Error>  {
+    return envy::prefixed("FITBIT_").from_env::<OAuth>()
+        .map(|oauth_config| 
             format!("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id={}&redirect_uri=http%3A%2F%2Flocalhost&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800", oauth_config.client_id)
-    );
-    println!("{:?}", uri)
+        );
 }
 
 #[derive(Deserialize, Debug)]
